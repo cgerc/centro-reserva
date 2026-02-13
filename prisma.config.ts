8b0dbc92baf1ced25874fa8bc46d575ef009597d
cgerc <gercanepacarmen@gmail.com>
2026-02-13 08:15:12 +0000

    env

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     filePath: 'prisma/schema.prisma',
   },
   datasource: {
-    // Aquí es donde Prisma 7 busca la conexión ahora
     url: process.env.DATABASE_URL,
   },
 });
